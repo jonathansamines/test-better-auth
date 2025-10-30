397169a06bcd66d2793e64ab31977e387f4d5119
Jonathan Samines <jn.samines@gmail.com>
2025-10-30 14:50:17 -0600

    chore: add access logs

---
+++ proxy.ts
@@ -0,0 +1,12 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export default async function proxy(request: NextRequest) {
+    console.log(`${request.method} ${request.url}`, request.nextUrl.pathname, await request.json());
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: '/api/:path*'
+};
+
+//  PUT /api/auth/scim/v2/Users/L9ZuWhfcDlW3WcFztNg946xIQmQaWRCo 400 in 1476ms (compile: 1463ms, render: 13ms)
