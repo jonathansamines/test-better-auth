8555fda4e3f55438f96e7523f393090d5c43e3a4
Jonathan Samines <jn.samines@gmail.com>
2025-10-30 15:25:44 -0600

    chore: properly rewrite headers

--- proxy.ts
+++ proxy.ts
@@ -1,8 +1,17 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export default async function proxy(request: NextRequest) {
-    console.log(`${request.method} ${request.url} ${request.headers.get('content-type')}`, await request.text(),);
-    return NextResponse.next();
+    const forwardedHeaders = new Headers(request.headers);
+
+    if (request.nextUrl.pathname.startsWith('/api/auth/scim')) {
+        forwardedHeaders.set('content-type', 'application/json');
+    }
+
+    console.log(`${request.method} ${request.url} ${request.headers.get('content-type')}`, await request.clone().json());
+
+    return NextResponse.next({
+        request: { headers: forwardedHeaders }
+    });
 }
 
 export const config = {
